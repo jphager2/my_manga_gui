e8e1ba0978aba42fe31ac8b64d9a04f5d2d6b661
jphager2 <jphager2@gmail.com>
2017-12-25 07:41:44 +0100

    Add styles

---
+++ src/MangaList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './MangaList.css';
+
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const {shell} = electron;
+
+function openExternal(e) {
+  const url = e.target.getAttribute('href');
+
+  e.preventDefault();
+  shell.openExternal(url);
+}
+
+function Manga(props) {
+  const url = props.href;
+  const slug = url.split('/').reverse()[0];
+  const image = url
+    .replace('www.mangareader.net', 's0.mangareader.net')
+    .replace(slug, `cover/${slug}/${slug}-l0.jpg`);
+
+  return (
+    <div className="Manga">
+      <div className="Manga-cover">
+        <img src={image} />
+      </div>
+      <div className="Manga-info">
+        <h2 className="Manga-title">{props.name}</h2>
+        <p className="Manga-description">
+          {props.readCount} chapters read of {props.chapterCount}
+        </p>
+        <p>
+          <a className="Manga-link" href={props.href} onClick={openExternal}>
+            Read online
+          </a>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+function MangaList(props) {
+  const { manga } = props;
+  const items = manga.map((item) => (
+    <Manga
+      key={item.id.toString()}
+      name={item.name}
+      href={item.uri}
+      chapterCount={item.total_count}
+      readCount={item.read_count}
+    />
+  ));
+
+  return (
+    <div className="MangaList">{items}</div>
+  );
+}
+
+export default MangaList;
